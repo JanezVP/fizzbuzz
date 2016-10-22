a1843dd169c5ee66302bc29bb3dba6958899b746
JanezVP <janez.voler@gmail.com>
2016-10-22 08:41:15 +0200

    Fizzbuzz

---
+++ 014_fizzbuzzstevec.py
@@ -0,0 +1,17 @@
+# -*- coding: UTF-8 -*-
+
+ugibanje = int (raw_input ("Vnesite stevilo med 1 in 100: "))
+
+for n in range (1, ugibanje + 1):
+
+    if n % 3 == 0 and n % 5 == 0:
+        print ("fizzbuzz")
+
+    elif n % 5 == 0:
+        print ("buzz")
+
+    elif n % 3 == 0:
+        print ("fizz")
+
+    else:
+        print (n)
